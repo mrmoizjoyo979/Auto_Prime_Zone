37e5fc813b22e931f5a7a9ca1f494eab793f9898
mrmoizjoyo979 <mrmoizjoyo979@gmail.com>
2025-02-07 23:06:53 +0500

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['cdn.sanity.io'],
-    },
+   eslint: {
+    ignoreDuringBuilds: true,
+   },
   };
   
   export default nextConfig;
